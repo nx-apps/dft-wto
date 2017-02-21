51cfbd98368e659c6b3347f94f9519e94253f8f4
tonsai-j <tonsai.j@nexts-corp.com>
2017-02-21 14:05:19 +0700

    update insert

--- app/controllers/importer/importer.controller.js
+++ app/controllers/importer/importer.controller.js
@@ -1,13 +1,6 @@
 exports.list = function (req, res) {
     var r = req.r;
     r.db('wto2').table('importer')
-        .merge(function (m) {
-            return {
-                seller: r.db('external').table('seller').getAll(m('tags')(0), {index: 'seller_tax_id'})
-                .coerceTo('array')
-                .without(['id','date_created'])
-            }
-        })
         .run()
         .then(function (result) {
             res.json(result)
@@ -16,8 +9,42 @@ exports.list = function (req, res) {
             res.json(err)
         })
 }
+exports.quotaList = function (req, res){
+    r.db('wto2').table('quota')
+    .run()
+    .then((result)=>{
+        res.json(result)
+    })
+    .error((err)=> {
+        res.json(err)
+    })
+}
 exports.insert = function (req, res) {
     var r = req.r;
+    var valid = req.ajv.validate('quota', req.body); 
+    var result = { result: false, message: null, id: null };
+    // console.log('===>',valid)
+    if(valid){
+        r.db('wto2').table('quota').insert(req.body)
+        .run()
+        .then((response)=>{
+            result.message = response;
+                if (response.errors == 0) {
+                    result.result = true;
+                    result.id = response.generated_keys;
+                }
+                res.json(result);
+        })
+        .error((err)=> {
+            result.message = err;
+                res.json(result);
+        })
+    } else {
+        result.message = req.ajv.errorsText()
+        res.json(result);
+    }
+    // console.log(rs)
+    
 }
 exports.update = function (req, res) {
     var r = req.r;
