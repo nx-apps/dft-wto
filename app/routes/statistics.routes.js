61a281b34108381e38c6d2f69312c8df71228c86
Nutthawut-Ice <moomookingza@gmail.com>
2017-05-18 15:17:17 +0700

    update stat

--- app/routes/statistics.routes.js
+++ app/routes/statistics.routes.js
@@ -5,4 +5,6 @@ module.exports = function (app) {
     app.route('/importer').get(controller.listimporter);
     app.route('/inquota').get(controller.listInqouta);
     app.route('/outquota').get(controller.listOutqouta);
+    app.route('/search_head').get(controller.search_list_head);
+    app.route('/search_detail').get(controller.search_list_detail);
 }
