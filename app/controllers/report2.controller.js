f23fa78b1433f0d6d9215ab55ba9808eb20c9571
Nutthawut-Ice <moomookingza@gmail.com>
2017-07-24 11:10:42 +0700

    update report5

--- app/controllers/report2.controller.js
+++ app/controllers/report2.controller.js
@@ -251,3 +251,23 @@ exports.report4 = function (req, res) {
 
 
 }
+exports.report5 = function (req, res) {
+    req.jdbc.query('mssql',
+        `select f.reference_code2 as reference_code2,
+	            c.company_name_th as company_name_th,
+	            f.contract_type as contract_type
+        from(
+	            select * 
+	            from f3   
+	            where reference_code2 = ? 
+        )f
+        left join(	
+	            select * 
+	            from company_info
+        )c on c.company_taxno = f.company_taxno`,
+        [req.query.reference_code2],
+        function (err, data) {
+            res.send(data)
+        }
+    )
+}
--- app/routes/report.routes.js
+++ app/routes/report.routes.js
@@ -4,4 +4,5 @@ module.exports = function (app) {
     app.get('/report2', controller.report2);
     app.get('/report3', controller.report3);
     app.get('/report4', controller.report4);
+    app.get('/report5', controller.report5); 
 }
