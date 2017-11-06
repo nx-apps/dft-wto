18ac2876da04873baeb40aeef8a4d8b11c607282
lissara.k <lissara.k@nexts-corp.com>
2017-11-06 17:16:58 +0700

    update report1 edi

--- app/controllers/report/edi.controller.js
+++ app/controllers/report/edi.controller.js
@@ -5,7 +5,7 @@ exports.report1 = function (req, res) {
     // if (typeof req.query.year_end !== 'string')
     //     req.query.year_end = Number(new Date().getFullYear()) + 1
 
-    req.jdbc.query("mssql", "exec sp_wto_rpt_due_date @startDate=?, @endDate=? ", [req.query.startDate, req.query.endDate],
+    req.jdbc.query("mssql", "exec sp_wto_rpt_due_date @startDate=?, @endDate=?, @status=? ", [req.query.startDate, req.query.endDate, req.query.status ],
         function (err, data) {
             data = JSON.parse(data);
             // const arrMonth = req.query.modelMonth.split(",").sort();
@@ -15,6 +15,7 @@ exports.report1 = function (req, res) {
             };
             param.START_DATE = req.query.startDate//rpt.getMonthName(Number(arrMonth[0])),
             param.END_DATE = req.query.endDate //rpt.getMonthName(Number(arrMonth[arrMonth.length - 1])),
+            param.STATUS = req.query.status
             // param.year = req.query.modelYear;
             param.CURRENT_DATE = new Date().toISOString().slice(0, 10);
             param.OUTPUT_NAME = param.CURRENT_DATE.replace(/-/g, '') + '_รายงานผู้ได้รับหนังสือรับรองแสดงการได้รับสิทธิชำระภาษีในโควตา WTO ที่ยังไม่ได้รายงานการนำเข้าภ'
